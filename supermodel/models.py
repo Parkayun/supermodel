1c573d8c3bd35a6bab4733d2ca41224e73ff81f5
Parkayun <iamparkayun@gmail.com>
2016-09-11 10:18:47 +0900

    Add POC

--- supermodel/models.py
+++ supermodel/models.py
@@ -1,11 +1,38 @@
+import requests
+
 from django.db import models
 from django.db.models.manager import Manager
 
 
 class RemoteManager(Manager):
 
+    def __make_object(self, fields):
+        obj = self.model()
+        for key, value in fields.items():
+            setattr(obj, key, value)
+        return obj
+
+    def __request(self, parameters):
+        url = self.model.host
+        return requests.post(url, data=parameters).json()
+
+    def _get_data(self, parameters=None):
+        parameters = parameters if isinstance(parameters, dict) else {}
+        data = self.__request(parameters)
+        results = None
+
+        if isinstance(data, list):
+            results = []
+            for fields in data:
+                results.append(self.__make_object(fields))
+
+        elif isinstance(data, dict):
+            results = self.__make_object(data)
+
+        return results
+
     def all(self):
-        pass
+        return self._get_data()
 
     def filter(self, *args, **kwargs):
         pass
